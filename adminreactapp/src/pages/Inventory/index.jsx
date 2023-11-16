b113941b99b3d7b520302499ba7437e9d3317fa6
JaleRehimli0909 <rehimlijale0909@gmail.com>
2023-11-16 23:25:00 +0400

    Inventory page ready..(added)

--- adminreactapp/src/pages/Inventory/index.jsx
+++ adminreactapp/src/pages/Inventory/index.jsx
@@ -1,12 +1,68 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./style.css";
-import { Typography } from "antd";
+import { Space, Table, Typography, Avatar, Rate } from "antd";
+import { getInventory } from "../../API/index";
 
 const Inventory = () => {
+  const [loading, setLoading] = useState(false);
+  const [dataSource, setDateSource] = useState([]);
+
+  useEffect(() => {
+    setLoading(true);
+    getInventory().then((res) => {
+      setDateSource(res.products);
+      setLoading(false);
+    });
+  }, []);
+
   return (
-    <div>
+    <Space size={20} direction="vertical">
       <Typography.Title level={4}>Inventory</Typography.Title>
-    </div>
+      <Table
+        loading={loading}
+        columns={[
+          {
+            title: "Title",
+            dataIndex: "title",
+          },
+          {
+            title: "Price",
+            dataIndex: "price",
+            render: (value) => <span>${value}</span>,
+          },
+          {
+            title: "Rating",
+            dataIndex: "rating",
+            render: (rating) => {
+              return <Rate value={rating} allowHalf />;
+            },
+          },
+          {
+            title: "Stock",
+            dataIndex: "stock",
+          },
+          {
+            title: "Thumbnail",
+            dataIndex: "thumbnail",
+            render: (link) => {
+              return <Avatar src={link} />;
+            },
+          },
+          {
+            title: "Brand",
+            dataIndex: "brand",
+          },
+          {
+            title: "Category",
+            dataIndex: "category",
+          },
+        ]}
+        dataSource={dataSource}
+        pagination={{
+          pageSize: 5,
+        }}
+      ></Table>
+    </Space>
   );
 };
 
